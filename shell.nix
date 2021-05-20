5cfd8d88e26974c79091eab8a76a76e3706c59fb
Brian W Bush <code@functionally.io>
2021-05-19 19:56:00 -0600

    set up haskell.nix environment

---
+++ shell.nix
@@ -0,0 +1,28 @@
+let
+  project = import ./default.nix;
+in
+  project.shellFor {
+
+    packages = ps: with ps; [
+#     mantis
+    ];
+
+    # Set the following to `false` do disable the lengthy building of documentation.
+    withHoogle = true;
+
+     # See overlays/tools.nix for more details
+    tools = {
+      cabal                   = "latest";
+      ghcide                  = "latest";
+      haskell-language-server = "latest";
+    # hdevtools               = "latest";
+    # hindent                 = "latest";
+      hlint                   = "latest";
+      pointfree               = "latest";
+    # pointfull               = "latest";
+    };
+
+    buildInputs = [ (import <nixpkgs> {}).git ];
+
+    exactDeps = true;
+  }
